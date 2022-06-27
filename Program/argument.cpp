442d7d28895df9d074f95c750bcb88f2062e68f7
plasmarad <73721329+plasmarad@users.noreply.github.com>
2022-06-27 18:16:34 -0400

    updated argument.cpp
    removed inaccurate comments, added some comments to explain.

--- Program/argument.cpp
+++ Program/argument.cpp
@@ -4,6 +4,7 @@ parser::parser(int argc, char** argv){
     _argv = argv; _argc = argc;
 }    
 
+// remove -- or - from command argv
 std::string parser::_format(char* argv){
     std::string arg = argv;
     if(arg.substr(0,2) == "--"){
@@ -43,19 +44,29 @@ parser::arg parser::_getArg(std::string arg_name){
     return _ARGS[0];
 }
 
-//rewrite this function. _arg_index is incorrect causing the value scraping to be wrong
 std::vector<char*> parser::getArgValues(std::string arg_name){
     std::vector<char*> _tmp_values;
+    
+    //offset for when looking in _argv
     int _arg_index = 0;
+    
+    // find the offset value by finding the argument's location in _argv
     for (int i = 0; i < _argc; i++) { 
+        // find the location of arg_name in _argv
         if(_argv[i] == _getArg(arg_name)._short || _argv[i] == _getArg(arg_name)._long) {
-            _arg_index = i += (_argv[i+1] == nullptr ? 0 : 1);
+            // if the next (i+1) cell in _argv is null: add 1 to i, otherwise add 0 to i 
+            _arg_index =  (i += ( (_argv[i+1] == nullptr) ? 0 : 1));
         } 
     }
+    
+    // returning the actual tokens
     for (int i = _arg_index;i < _argc; i++){
+        // if it's not an argument: push to the vector
         if  (_argv[i][0] != '-') _tmp_values.push_back(_argv[i]);
+        // otherwise: return all the arguments.
         if  (_argv[i][0] == '-') return _tmp_values;
     }
+    // return the arguments in the form of a vector of chars (char*)
     return _tmp_values;
 }
 
